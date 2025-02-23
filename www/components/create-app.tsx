fed45e9a1e9e9f2e3929de31d737cd2cb7572d15
nabinkhair42 <nabinkhair12@gmail.com>
2025-02-23 17:07:48 +0545

    Update create-app component with npm package availability toast

--- www/components/create-app.tsx
+++ www/components/create-app.tsx
@@ -7,7 +7,7 @@ export default function CreateAppComponent() {
   return (
     <div className="flex flex-row items-center gap-2 bg-muted p-2 rounded-md mt-8 cursor-copy" onClick={() => {
         navigator.clipboard.writeText("npx create-pestjs-app <project-directory>");
-        toast.success("Copied to clipboard");
+        toast.warning("npm package will be available soon");
       }}>
         <TerminalSquareIcon className="w-5 h-5 sm:mr-1 mt-0.5" />
         {"npx create-pestjs-app <project-directory>"}
